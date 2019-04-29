23edac00fc4631787561f40345fe2fdd46409c9b
David Maier <david@nosqlgeeks.de>
2019-04-29 21:16:12 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,22 @@
+print("Starting Hello World service ....");
+
+# Imports
+from flask import Flask
+from redis import Redis
+
+## Init
+app = Flask(__name__)
+redis = Redis(host='redis-18210.c12.us-east-1-4.ec2.cloud.redislabs.com', port=18210, password='49FSIbUuXjoFzC5tzM2k1METm2r3vBsK')
+
+## Test
+redis.set("hello", "Hello World again!");
+
+## Routes
+@app.route("/hello")
+def hello():
+	res = redis.get("hello")
+	return res
+    
+if __name__ == "__main__":
+	app.debug = False
+	app.run(host='0.0.0.0', port=5000)

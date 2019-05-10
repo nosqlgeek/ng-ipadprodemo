438de58732efbfeec972de5025398a79730d6e04
David Maier <david@nosqlgeeks.de>
2019-05-10 09:27:54 +0200

    Better result formatting

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ redis = Redis(host=host, port=port, password=pwd, charset="utf-8", decode_respon
 #redis.flushdb();
 
 ## Routes
+
+### Home page
 @app.route("/home")
 def home():
 	# View constants
@@ -31,6 +33,7 @@ def home():
 	DESC="Welcome to this simple Redis demo application. This application allows you to check the database connectivity, access some database configuration details and to execute Redis commands."
 	return render_template('home.html', title=TITLE, desc=DESC)
 
+### Database info
 @app.route("/db/info")
 def dbinfo():
 	# View constants
@@ -41,36 +44,30 @@ def dbinfo():
 	
 	try:
 		result = redis.info()
-		#print(status)
 		for k in result:
 			item = {}
 			item["key"] = k
 			item["value"] = result[k]
-			items.append(item)   
+			items.append(item)
+		return render_template('dbinfo.html', title=TITLE, desc=DESC, items=items, status="Success")   
 	except RedisError as err:
-		item = {}
-		item["key"] = "error"
-		item["value"] = err
-		items.append(item) 
-	
-	return render_template('dbinfo.html', title=TITLE, desc=DESC, items=items)
+		return render_template('dbinfo.html', title=TITLE, desc=DESC, error=err) 
 
+### Test connection
 @app.route("/db/test")
 def dbtest():
 	# View constants
 	TITLE="Test Connectivity"
 	DESC="Find the status of the connectivity test below ..."
 	
-	status = "Not connected!"
-	
 	try:
 		redis.set("db:test", "Database connectivity works as expected!");
 		status = redis.get("db:test")
+		return render_template('dbtest.html', title=TITLE, desc=DESC, status=status)
 	except RedisError as err:
-		status = err
+		return render_template('dbtest.html', title=TITLE, desc=DESC, error=err)
 	
-	return render_template('dbtest.html', title=TITLE, desc=DESC, status=status)
-
+### Execute a command
 @app.route("/db/exec")
 def execcmd():
 	# View constants
@@ -84,13 +81,33 @@ def execcmd():
 		print("Trying to execute: {}".format(cmd))
 		try:
 			result = redis.execute_command(cmd)
-			status = "Success"
-			return render_template('execcmd.html', title=TITLE, desc=DESC, cmd=cmd, status=status, result=result)
+			
+			# Nicer result output
+			lines=[]
+			if (isinstance(result, list)):
+				# Adding this block for SCAN which returns the cursor followed by a list
+				if len(result) == 2 and isinstance(result[1],list):
+					lines = result[1]
+					lines.insert(0, result[0])
+				else:
+					# Output of a list, i.e. KEYS *
+					lines=result;
+			elif (isinstance(result, dict)):
+				# Adding support for a dict result (i.e. INFO)
+				for k in result:
+					val = "{} : {}".format(k, result[k])
+					lines.append(val)
+			else:
+				# Everything else is just defaulting to a String output
+				lines = str(result).splitlines()
+		
+			return render_template('execcmd.html', title=TITLE, desc=DESC, cmd=cmd, status='Success', lines=lines)
 		except RedisError as err:
 			return render_template('execcmd.html', title=TITLE, desc=DESC, cmd=cmd, error=err)
 	
 	return render_template('execcmd.html', title=TITLE, desc=DESC)
 
+### Start page
 @app.route("/")
 def root():
 	return redirect('/home')
